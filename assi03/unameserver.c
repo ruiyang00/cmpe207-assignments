e6a43cdbf43e47c976e42b5ff260bf9a1db08f6c
Ubuntu <ubuntu@ip-172-31-7-176.us-west-1.compute.internal>
2021-03-05 04:04:42 +0000

    fix characters bug for both udp&tcp for unameserver

--- assi03/unameserver.c
+++ assi03/unameserver.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,6 +15,19 @@
 #define OFF 0
 #define BUFFER_LEN 2048
 
+struct UnameMap{
+	int sbit;
+	int abit;
+	int nbit;
+	int rbit;
+	int vbit;
+	int mbit;
+	int pbit;
+	int ibit;
+	int obit;
+	int errorbit;
+};
+
 void validateArgv(int argc,char **argv, int *portnum, int *trans_flag);
 void exitWithError();
 void exitSysWithError(char *call);
@@ -21,6 +35,7 @@ void udpServer(char **argv, int *port_flag);
 void tcpServer(char **argv, int *port_flag);
 void unameCaller(char *buffer);
 void validatePortArg(char *buffer);
+void validateUnameArg(char *buffer, struct UnameMap *m);
 
 int main(int argc, char **argv) {
 
@@ -42,7 +57,69 @@ int main(int argc, char **argv) {
 	return 0;
 }
 void udpServer(char **argv, int *port) {
+	int sockfd, len;
+    struct sockaddr_in servaddr, clientaddr;
+    char buffer[BUFFER_LEN];
+    
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd == -1) {
+        exitSysWithError("socket()");
+    }
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(*port==OFF) {
+        servaddr.sin_port = htons(0);
+    }
+    if(*port == ON) {
+        validatePortArg(argv[3]);
+    }
+    if(*port == ON && atoi(argv[3]) <= 2000) {
+
+        printf("port=%d\n", *port);
+        exitWithError("port number too small. Please input port number greater than 2000");
+    }
+
+    if(*port == ON && atoi(argv[3]) > 2000) {
+        servaddr.sin_port = htons(atoi(argv[3]));
+    }
+
+    //TBD:bind
+    int ret = bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    if(ret != 0) {
+        exitSysWithError("bind()");
+    }
 	
+	// stdio output the port in terminal
+	len = sizeof(sockfd);
+    if(getsockname(sockfd, (struct sockaddr*) &servaddr, &len) == -1) {
+        exitWithError("getsockname()");
+    }
+
+    if(*port == OFF) {
+        printf("Server listening at port number: %u\n", ntohs(servaddr.sin_port));
+    }else if(*port == ON) {
+        printf("Server listening...\n");
+    }
+
+
+	while(1) {
+        len = sizeof(clientaddr);
+
+		//TBD: recvfrom()
+		ret = recvfrom(sockfd, buffer, BUFFER_LEN, 0, &clientaddr, &len);
+        buffer[ret] = '\0';
+        unameCaller(buffer);
+		//TBD: sendto()
+        sendto(sockfd, buffer, strlen(buffer), 0, &clientaddr, len);
+    }
+	
+	//TBD: close()
+    close(sockfd);
+    exit(0);
+	
+
+		
 }
 
 void tcpServer(char **argv, int *port) {
@@ -105,15 +182,19 @@ void tcpServer(char **argv, int *port) {
 		}
 
 		//TBD: read()/recv()
+		memset(buffer, 0, sizeof buffer);
 		while((ret = recv(client_sockfd, buffer, BUFFER_LEN, 0)) > 0) {
-			buffer[ret] = '\0';
+			buffer[strlen(buffer)] = '\0';
+			printf("strlen=%ld\n", strlen(buffer));
 			printf("received message=%s\n", buffer);
+			printf("ret=%d\n",ret);
 			unameCaller(buffer);
+			printf("buffer=%s\n",buffer);
 			write(client_sockfd, buffer, strlen(buffer));
 		}
 	
 		if(ret == 0) {
-			printf("connection close with client%s...\n", inet_ntoa(client.sin_addr));
+			printf("connection close with client: %s...\n", inet_ntoa(client.sin_addr));
 		} else if(ret == -1) {
 			exitSysWithError("recv()");
 		}
@@ -123,18 +204,196 @@ void tcpServer(char **argv, int *port) {
 }
 
 void unameCaller(char *buffer){
-	printf("unameCller()\n");
+
 	struct utsname ustr;
+	char *temp;
 	int ret;
-	if(strcmp(buffer, "-s") == 0) {
-		ret = uname(&ustr);
-		if(ret == -1) {
-			exitSysWithError("uname()");
-		}
-		printf("ustr=%s", ustr.sysname);	
-		buffer == ustr.sysname;
+	struct UnameMap m;
+	int len = 0;
+	
+
+	memset(&m, 0, sizeof(m));
+	ret = uname(&ustr);
+    
+
+	if(ret == -1) {
+		exitSysWithError("uname()");
 	}
 
+	validateUnameArg(buffer, &m);
+	memset(buffer, 0, sizeof buffer);
+
+	/*if we don't return right here that means all inputs are validate
+	*	else we return a error message to the client notifying that client's input arg
+	* 	is not one of a|s|n|r|v|m|p|o.
+	*/
+	if(m.errorbit == 1) {
+		strcpy(buffer, "we only accept a|s|n|r|v|m|p|o arg inputs\0");
+		printf("errorbit:%s\n", buffer);
+		return;
+	}
+
+	/*1)if we reach here, it means that client's args are all validated
+	* 2)we check if use's args have 'a'. If we see a-bit is on then we fill buffer with 
+	*   all info return by uname syscall and return. Bsically a means return all.
+	* 
+	*/
+	if(m.abit == 1) {
+		/*concatinating system name to the buffer*/
+        strcat(buffer,ustr.sysname);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+		printf("len=%d\n", len);
+
+
+		/*concatinating server name to the buffer*/
+        strcat(buffer,ustr.nodename);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+		
+		/*concatinating release versiov to the buffer*/
+        strcat(buffer, ustr.release);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+
+
+		/*concatinating os distro to the buffer*/
+        strcat(buffer, ustr.version);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+
+		/*concatinating machine to the buffer
+        len += strlen(ustr.machine);
+        strcat(buffer, ustr.machine);
+        buffer[len++] = ' ';
+		*/
+	
+		
+		/*concatinating processor to the buffer
+        len += strlen(ustr.machine);
+        strcat(buffer, ustr.machine);
+        buffer[len] = ' ';*/
+
+		/*concatinating hardware archit to the buffer*/
+        strcat(buffer, ustr.machine);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+
+		/*concatinating os to the buffer*/
+        strcat(buffer, ustr.sysname);
+		strcat(buffer, "\0");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+
+
+		return;
+	}
+	
+
+	/*if we reach here, it means that we don't have 'a' as a input arg.
+	* We need to fill in whatever the input args are in the buffer.
+	*/
+	if(m.sbit == 1) {
+		strcat(buffer,ustr.sysname);
+        strcat(buffer, " ");
+        printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+	}
+
+	if(m.nbit == 1) {
+        strcat(buffer, ustr.nodename);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+	}
+	if(m.rbit == 1) {
+		strcat(buffer, ustr.release);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+	}
+	if(m.vbit == 1) {
+        strcat(buffer, ustr.version);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+    }
+	if(m.mbit == 1) {
+        strcat(buffer, ustr.machine);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+    }
+	if(m.pbit == 1) {
+        strcat(buffer, ustr.machine);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+    }
+	if(m.ibit == 1) {
+        strcat(buffer, ustr.machine);
+		strcat(buffer, " ");
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+    }
+	if(m.obit == 1) {
+        strcat(buffer,ustr.sysname);
+		strcat(buffer, " ");
+		buffer[len] = ' ';
+		printf("buffer=%s\n", buffer);
+        printf("len=%d\n", len);
+	}
+	strcat(buffer, "\0");
+	printf("len=%d\n", len);
+	printf("buffer=%s\n", buffer);
+
+
+}
+
+void validateUnameArg(char *b, struct UnameMap *m) {
+
+	for(int i = 0; b[i] != '\0'; i++) {
+		char c = b[i];
+		printf("b[%d]=%s\n",i, &c);
+		printf("strcmp=%d\n", strcmp(&c, "n"));
+		if('a' == c) {
+			printf("strcmp=%d\n",strcmp("a", &c));
+			m->abit = 1;
+		} else if(c == 's') {
+			printf("strcmp=%d\n",strcmp("s", &c));
+			m->sbit = 1;
+		} else if(c == 'n') {
+			printf("strcmp=%d\n",strcmp("n", &c));
+            m->nbit = 1;
+        } else if(c == 'r') {
+			printf("strcmp=%d\n",strcmp("r", &c));
+            m->rbit = 1;
+        } else if(c == 'v') {
+			printf("strcmp=%d\n",strcmp("v", &c));
+            m->vbit = 1;
+        } else if(c == 'm') {
+			printf("strcmp=%d\n",strcmp("m", &c));
+            m->mbit = 1;
+        } else if(c == 'p') {
+			printf("strcmp=%d\n",strcmp("p", &c));
+            m->pbit = 1;
+        } else if(c == 'o') {
+			printf("strcmp=%d\n",strcmp("o", &c));
+            m->obit = 1;
+        } else if(c == 'i') {
+			printf("strcmp=%d\n",strcmp("i", &c));
+            m->ibit = 1;
+		} else if(c != ' '){
+			return;
+		} else {
+			m->errorbit=1;
+			return;
+		}
+	}
 }
 
 void validatePortArg(char *buffer) {
